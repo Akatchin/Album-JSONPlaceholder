c5fd1a3aa114855b14f446fff6e243a68644be55
Thiago <thiago.jp.221@gmail.com>
2023-12-24 16:44:54 -0300

    Adding timer in rendering of photos

--- src/pages/Photos/AlbumPhoto.tsx
+++ src/pages/Photos/AlbumPhoto.tsx
@@ -4,9 +4,11 @@ import { useParams, Link } from "react-router-dom"
 import styled from "../../styles/Photos.module.css"
 import { BackButton } from "../../button/BackButton"
 import { Footer } from "../footer/Footer"
-
+import loading from "../../assets/loading.gif";
 
 export const AlbumPhoto = () => {
+
+    const [finishedTimeOut, setFinishedTimeOut] = useState(false)
 
     type Props = {
         id: string
@@ -18,7 +20,6 @@ export const AlbumPhoto = () => {
 
     const { id } = useParams()
 
-
     useEffect(() => {
         api.getPhoto(id)
             .then((res) => {
@@ -27,27 +28,43 @@ export const AlbumPhoto = () => {
             })
     }, [id])
 
-    return (
-        <div>
-            <div className={styled.backButton}>
-            <BackButton />
+    useEffect(() => {
+        const id: ReturnType<typeof setTimeout> = setTimeout(() => {
+          setFinishedTimeOut(true)
+        }, 3000)
+        return () => clearTimeout(id)
+      }, [])
+      
+      if(!finishedTimeOut) {
+        return(
+            <div className={styled.loadingDiv}>
+              <img width="100px" height="100px"className={styled.loading} src={loading} alt="" />
+              <p>Carregando...</p>
             </div>
-            <div className={styled.container}>
-                {imgUrl.map((obj: Props) => {
-                    Object.keys(obj)
-                    return (
-                            <div className={styled.album}>
-                                <div className={styled.title}>
-                                    <p className={styled.titleText}>{obj.title}</p>
-                                </div>
-                                <div className={styled.photo} >
-                                    <Link to={`/photos/${id}`}><img className={styled.img} src={obj.thumbnailUrl} alt="" width="200px" height="200px" /></Link>
-                                </div>
-                            </div>
-                    )
-                })}
-            </div>
-            <Footer/>
-        </div>
-    )
+          )
+      } else {
+          return (
+              <div>
+                  <div className={styled.backButton}>
+                      <BackButton />
+                  </div>
+                  <div className={styled.container}>
+                      {imgUrl.map((obj: Props) => {
+                          Object.keys(obj)
+                          return (
+                              <div className={styled.album}>
+                                  <div className={styled.title}>
+                                      <p className={styled.titleText}>{obj.title}</p>
+                                  </div>
+                                  <div className={styled.photo} >
+                                      <Link to={`/photos/${id}`}><img className={styled.img} src={obj.thumbnailUrl} alt="" width="200px" height="200px" /></Link>
+                                  </div>
+                              </div>
+                          )
+                      })}
+                  </div>
+                  <Footer />
+              </div>
+          )
+      }
 }
